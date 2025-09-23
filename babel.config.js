16f2a98d336c97e1b8efe1be03cf91c5649fd6d6
Benno <Benno Evers>
2025-09-23 02:26:50 +0200

    Enable console logging

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,10 @@ module.exports = function (api) {
     presets: ['babel-preset-expo'],
     env: {
       production: {
-        plugins: ["transform-remove-console"],
+        plugins: [
+          // Temporarily disable console removal for debugging
+          // "transform-remove-console",
+        ],
       },
     },
     plugins: [
